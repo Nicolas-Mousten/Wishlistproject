b69161cad10518b9533960f17029f78bb979cdba
Yaimee <37310514+Yaimee@users.noreply.github.com>
2022-04-01 10:35:30 +0200

    Databaser ændringer

--- src/main/java/com/example/wishlist/WishlistApplication.java
+++ src/main/java/com/example/wishlist/WishlistApplication.java
@@ -8,10 +8,10 @@ import com.example.wishlist.repositores.Database;
 
 @SpringBootApplication
 public class WishlistApplication {
-    static Database db1 = new Database("jdbc:mysql://127.0.0.1:3306/wish_list","root","dk5gmjpfmebf");
+    /*static Database db1 = new Database("jdbc:mysql://127.0.0.1:3306/wish_list","root","");*/
     public static void main(String[] args) {
         Database db = new Database("jdbc:mysql://127.0.0.1:3306/wishlists","root","");
-        /*db.connectDB();*/
+        db.connectDB();
         /*db.insertIntoProduct(new Wish(5687,"computer",145,1578));*/
         SpringApplication.run(WishlistApplication.class, args);
     }
