cfba82aef565c2fce0c8c5861e551929e0008672
Nicolas-Mousten <nicolas.mousten@gmail.com>
2022-04-03 12:30:10 +0200

    Login UserService Done

--- src/main/java/com/example/wishlist/WishlistApplication.java
+++ src/main/java/com/example/wishlist/WishlistApplication.java
@@ -1,5 +1,6 @@
 package com.example.wishlist;
 
+import com.example.wishlist.repositores.Database;
 import com.example.wishlist.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WishlistApplication {
-    public static Database db = new Database("jdbc:mysql://127.0.0.1:3306/wishlists","root","dk5gmjpfmebf");
+    public static Database db = new Database("jdbc:mysql://127.0.0.1:3306/wishlists","root","");
     public static void main(String[] args) {
 
         /*db.insertIntoProduct(new Wish(5687,"computer",145,1578));*/
