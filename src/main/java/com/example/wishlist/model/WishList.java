37e27013f32526e742360dac7f02aaff8de9a65c
Yaimee <37310514+Yaimee@users.noreply.github.com>
2022-04-04 14:01:16 +0200

    Databaser ændringer

--- src/main/java/com/example/wishlist/model/WishList.java
+++ src/main/java/com/example/wishlist/model/WishList.java
@@ -3,22 +3,22 @@ package com.example.wishlist.model;
 import java.util.ArrayList;
 
 public class WishList {
-    private ArrayList<Wish> wishes = new ArrayList<>();
-    int userId;
+    private ArrayList<Product> products = new ArrayList<>();
+    int userEmail;
     String wishListName;
     int wishListId;
 
-    private WishList(int userId, String wishListName, int wishListId) {
-        this.userId = userId;
+    private WishList(int userEmail, String wishListName, int wishListId) {
+        this.userEmail = userEmail;
         this.wishListName = wishListName;
         this.wishListId = wishListId;
     }
 
-    public void addToWishList(Wish wish) {
-        wishes.add(wish);
+    public void addToWishList(Product product) {
+        products.add(product);
     }
 
-    public Wish getWish(int index) {
-        return wishes.get(index);
+    public Product getProduct(int index) {
+        return products.get(index);
     }
 }
