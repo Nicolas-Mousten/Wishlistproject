9ce0bcc263fce320886c28aab89b7f43e5482257
Nicolas-Mousten <nicolas.mousten@gmail.com>
2022-04-05 10:50:07 +0200

    SQL

--- SQL Code/Wishes DDL.sql
+++ SQL Code/Wishes DDL.sql
@@ -4,27 +4,27 @@ use wishlists;
 drop table if exists `product`;
 drop table if exists`wish_list`;
 
-CREATE TABLE IF NOT EXISTS `wish_list` (
-`wish_list_id` INT(10) PRIMARY key AUTO_INCREMENT,
-`wish_list_name` VARCHAR(200),
-foreign key (`email`) references `user` (`email`)
-);
-
-CREATE TABLE IF NOT EXISTS `product` (
-`product_id` INT(10) PRIMARY KEY,
-`product_name` VARCHAR(200),
-`product_price` DOUBLE (10,5),
-`isReserved` TINYINT(1),
-`wish_list_id` INT(10),
-foreign key(`wish_list_id`) references `wish_list`(`wish_list_id`)
-);
 
 CREATE TABLE IF NOT EXISTS `user` (
     `email` VARCHAR(50) PRIMARY KEY,
     `password` VARCHAR(12)
-);
+    );
+CREATE TABLE IF NOT EXISTS `wish_list` (
+    `wish_list_id` INT(10) PRIMARY key AUTO_INCREMENT,
+    `wish_list_name` VARCHAR(200),
+    `email` VARCHAR(50),
+    foreign key (`email`) references `user` (`email`)
+    );
+CREATE TABLE IF NOT EXISTS `product` (
+    `product_id` INT(10) PRIMARY KEY,
+    `product_name` VARCHAR(200),
+    `product_price` DOUBLE (10,5),
+    `isReserved` TINYINT(1),
+    `wish_list_id` INT(10),
+    foreign key(`wish_list_id`) references `wish_list`(`wish_list_id`)
+    );
 CREATE TABLE IF NOT EXISTS `wish_list_invites` (
     `wish_list_id` INT(10),
     `email` VARCHAR(50),
     foreign key(`email`) references `user`(`email`)
-);
+    );
