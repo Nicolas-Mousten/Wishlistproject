309317304c30a12b8ded6b3c02309c19e315e056
Nicolas-Mousten <nicolas.mousten@gmail.com>
2022-04-05 10:59:17 +0200

    Online Database acces

--- src/main/java/com/example/wishlist/WishlistApplication.java
+++ src/main/java/com/example/wishlist/WishlistApplication.java
@@ -9,13 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WishlistApplication {
-    public static Database db = new Database("jdbc:mysql://127.0.0.1:3306/wishlists","root","");
+    public static Database db = new Database("jdbc:mysql://keawishlist.mysql.database.azure.com:3306/wishlists","KeaGruppe3@keawishlist","3GruppeKea");
     public static void main(String[] args) {
 
         /*db.insertIntoProduct(new Wish(5687,"computer",145,1578));*/
         SpringApplication.run(WishlistApplication.class, args);
         //UserService.emailIsTaken("tobias@gmail.com","iAmStupid");             //Works
         //UserService.logIn("tobias@gmail.com","iStupid");                      //Works
+
     }
 
 }
